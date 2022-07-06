6e6d3ad32a10e4dc3e75bc03e4de075dc4527644
zhany <zhanybeksalgarin@gmail.com>
2022-07-06 12:10:23 -0700

    Initial Commit

---
+++ Tic-Tac-Toe/Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeApp.swift
+//  Tic-Tac-Toe
+//
+//  Created by zhanybek salgarin on 7/6/22.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
